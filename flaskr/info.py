d640dba71914b702cf2e8e033ba8eff6bbcb67e7
Abhishek Bhardwaj <abshkbh@gmail.com>
2023-03-27 11:22:10 -0700

    Getting expression in the evaluate endpoint

--- flaskr/info.py
+++ flaskr/info.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, abort
 from flaskr import server_state
 
 # TODO: Move global object initialization in init to be used by other endpoint collections as well.
@@ -18,3 +18,23 @@ def create_session():
 
     session_id = SERVER_STATE.create_new_session()
     return jsonify({'id': session_id})
+
+
+@bp.route('/evaluate/<int:session_id>', methods=['POST'])
+def evaluate(session_id):
+    """Evaluates an expression at the server with id=|id|."""
+    request_json = request.get_json()
+    if not request_json:
+        abort(400, {'error': 'no JSON data in the request'})
+
+    expression = request_json.get('expression')
+    if not expression:
+        abort(400, {'error': 'no expression provided to evaluate'})
+
+    try:
+        session = SERVER_STATE.get_session(session_id)
+    except ValueError as _:
+        abort(400, {'error': f'invalid session id={id}'})
+
+    print(f'Going to evaluate expression={expression} on session id={id}')
+    return jsonify()
