001f31a65a1cbcd8b308fab114d8e8992033ab06
Abhishek Bhardwaj <abshkbh@gmail.com>
2023-03-27 10:21:07 -0700

    Modify comment on / endpoint

--- flaskr/info.py
+++ flaskr/info.py
@@ -5,10 +5,11 @@ bp = Blueprint('info', __name__, url_prefix='/')
 
 @bp.route('/', methods=['GET'])
 def root():
-    """Runs ls and returns the result"""
-    return jsonify({'output' : 'Welcome to LUA evaluation server'})
+    """Returns a sanity check about the server running."""
+    return jsonify({'output': 'Welcome to LUA evaluation server'})
+
 
 @bp.route('/create_session', methods=['POST'])
 def add():
     """Runs ls and returns the result"""
-    return jsonify({'output' : 'dummy_file.txt'})
+    return jsonify({'output': 'dummy_file.txt'})
