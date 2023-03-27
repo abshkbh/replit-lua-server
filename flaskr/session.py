fbae08de70997ba0cf0e37c150970af020640ad2
Abhishek Bhardwaj <abshkbh@gmail.com>
2023-03-27 10:58:51 -0700

    Instantiate a LuaRuntime object in each session

--- flaskr/session.py
+++ flaskr/session.py
@@ -1,6 +1,10 @@
+import lupa
+from lupa import LuaRuntime, LuaSyntaxError
+
+
 class Session():
     """
-    Maintains the state associated with a server and isn't persisted in memory.
+    Maintains the state associated with a server and isn't persisted in memory. Currently, is only used to evaluate Lua expressions.
     """
 
     def __init__(self, session_id):
@@ -8,4 +12,4 @@ class Session():
         self.__session_id = session_id
 
         # The object to evaluate the expression.
-        self.__eval_object = None
+        self.__eval_object = LuaRuntime()
